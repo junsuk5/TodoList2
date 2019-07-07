0ccebeb5128eece374a50d86fe9979d4b0305335
Junsuk Oh <a811219@gmail.com>
2019-07-07 14:25:58 +0900

    리스트 2

--- app/src/main/java/com/exmaple/todolist/TodoListFragment.kt
+++ app/src/main/java/com/exmaple/todolist/TodoListFragment.kt
@@ -8,8 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
+import com.exmaple.todolist.databinding.ItemTodoBinding
 import com.exmaple.todolist.db.AppDatabase
+import com.exmaple.todolist.models.Todo
 import kotlinx.android.synthetic.main.fragment_todo_list.*
 
 class TodoListFragment : Fragment() {
@@ -39,6 +43,40 @@ class TodoListFragment : Fragment() {
         // 전체 데이터 가져오기
         val todos = db.todoDao().getAll()
         Log.d("TodoListFragment", todos.toString())
+
+        // Adapter
+        val adapter = TodoAdapter {
+
+        }
+        adapter.items = todos
+
+        // View
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = adapter
     }
 
+}
+
+class TodoAdapter(private val clickListener: (item: Todo) -> Unit) :
+    RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    var items = listOf<Todo>()
+
+    class TodoViewHolder(val binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_todo, parent, false)
+        val viewHolder = TodoViewHolder(ItemTodoBinding.bind(view))
+        view.setOnClickListener {
+            clickListener.invoke(items[viewHolder.adapterPosition])
+        }
+        return viewHolder
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.binding.todo = items[position]
+    }
 }
